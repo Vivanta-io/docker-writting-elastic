510ff8c70ee94bbdd20b697483c9357f9383e99d
Luis Ruelas <luis@vivanta.io>
2025-01-20 15:58:40 -0600

    Refactor Lambda function to improve Elasticsearch document indexing and update Docker Compose port configuration

--- code/lambda_function.py
+++ code/lambda_function.py
@@ -1,31 +1,52 @@
+import os
 from elasticsearch import Elasticsearch
 import json
 
+ELASTIC_ENDPOINT = os.environ.get('ELASTIC_ENDPOINT')
+ELASTIC_PORT = os.environ.get('ELASTIC_PORT')
+ELASTIC_STRING = f'{ELASTIC_ENDPOINT}:{ELASTIC_PORT}'
+ELASTIC_KEY = os.environ.get('ELASTIC_KEY')
+
+def __send_to_elastic(index_name, doc):
+    es_client = Elasticsearch(ELASTIC_STRING, api_key=ELASTIC_KEY) 
+    response = es_client.index(index=index_name, document=doc)
+    print(f'Indexing document in {index_name} index: {type(doc)}', doc)
+    return response
+
+def __send_doc_splitted(index_name:str = None, doc:dict = None):
+    if not index_name:
+        raise ValueError('Index name is required')
+    if not doc:
+        raise ValueError('Document is required')
+    if 'payload' in doc:
+        if isinstance(doc['payload'], list):
+            for payload in doc['payload']:
+                doc_to_send = doc.copy()
+                doc_to_send['payload'] = payload
+                __send_to_elastic(index_name, doc_to_send)
+        else:
+            __send_to_elastic(index_name, doc)
+    else:
+        __send_to_elastic(index_name, doc)
+    return doc
+
+
 def lambda_handler(event, context):
-    """
-    Lambda function to index multiple documents in Elasticsearch from SQS.
-
-    Args:
-        event: SQS event containing multiple records.
-        context: Lambda context object.
-
-    Returns:
-        dict: Response with status code and message.
-    """
-
     try:
         # Connect to Elasticsearch
-        es_client = Elasticsearch(hosts=[{'host': 'your-elasticsearch-endpoint', 'port': 9200}]) 
+        print('ELASTIC_STRING', ELASTIC_STRING)
+        print('ELASTIC_KEY', ELASTIC_KEY)
+        es_client = Elasticsearch(ELASTIC_STRING, api_key=ELASTIC_KEY) 
+        # API key should have cluster monitor rights
+        info = es_client.info()
 
         # Process each record in the SQS event
         for record in event['Records']:
             message = record['body']
             message_json = json.loads(message)
-            index_name = message_json.get('index', 'default_index')  # Get index from message
-            doc = message_json.get('doc', {})  # Get document from message
-
-            # Index document in Elasticsearch
-            response = es_client.index(index=index_name, document=doc)
+            doc = message_json.get('doc', {})
+            index_name = message_json.get('index', {})
+            doc = __send_doc_splitted(index_name, doc)
 
         return {
             'statusCode': 200,
